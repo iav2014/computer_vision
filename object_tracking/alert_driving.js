05e6aec5df5f3a4cd7632e773f3e7415a58323c2
Ignacio Ariza Victoria <ignacio.ariza.victoria@everis.com>
2018-12-18 15:06:58 +0100

    alert driving, detect fatigue while driving, working,etc

--- object_tracking/alert_driving.js
+++ object_tracking/alert_driving.js
@@ -1,15 +1,15 @@
 /**
- * advertising_recognition example
- * this example code using opencv and different classifiers to
- * detect object in video or webcam devices
+ * alert driving example
+ * this example code using opencv and eye classifier to detect
+ * fatigue while driving (one or two eyes closed)
+ * sending beep in  run console mode
+ * using webcam device
  * (c) Nacho Ariza Nov 2018
  * MIT License
  */
-
+let beep = require('beepbeep')
 const cv = require('opencv4nodejs');
-const robot_soccer_video = './object_tracking/video/robot_soccer.mp4';
-const ball_video = './object_tracking/video/ball.mp4';
-const camera = new cv.VideoCapture(robot_soccer_video); // or ball_video
+const camera = new cv.VideoCapture(0); // or
 const minDetections = 10;
 const classifiers = {
 	face: './object_tracking/classifiers/face.xml', // detect human face classifier
@@ -18,7 +18,7 @@ const classifiers = {
 	ball: './object_tracking/classifiers/ball_cascade.xml' // detect adidas 1978 model classifier
 };
 // cv.CascadeClassifier(0); // 0 to read from webcam device (macosx)
-const classifier = new cv.CascadeClassifier(classifiers.ball);
+const classifier = new cv.CascadeClassifier(classifiers.eye);
 // BLUE/GREEN/RED
 const green = new cv.Vec(0, 255, 0);
 const red = new cv.Vec(0, 0, 255);
@@ -27,20 +27,33 @@ const blue = new cv.Vec(255, 0, 0);
 const white = new cv.Vec(255, 255, 255);
 let face_region_color = blue;
 let text_color = green;
-
+let counter = 0;
 const detect = function (frame) {
 	const result = classifier.detectMultiScale(frame);
-	result.objects.forEach((faceRect, i) => {
-		if (result.numDetections[i] < minDetections) {
-			//console.log(result.numDetections[i], minDetections);
-			return;
-		}
-		const rect = cv.drawDetection(
-			frame,
-			faceRect,
-			{color: yellow, segmentFraction: 4}
-		);
-	});
+	//console.log('result.objects.length:',result.objects.length);
+	if(counter>10){
+		console.log('BEEP !!! !!!');
+		beep(1); // console beep
+	}
+	if (result.objects.length < 2) { // eyes not detected os one of them closed,,,
+		//console.log('counter:' + counter);
+		counter++;
+	} else { // eyes detected !
+		result.objects.forEach((faceRect, i) => {
+			if (result.numDetections[i] < minDetections) {
+				//console.log('=>',result.numDetections[i], minDetections, counter);
+				return;
+			} else {
+				counter = 0; // two eye detected, reset counter
+				const rect = cv.drawDetection(
+					frame,
+					faceRect,
+					{color: yellow, segmentFraction: 4}
+				);
+			}
+			
+		});
+	}
 	return frame;
 };
 
@@ -54,7 +67,7 @@ while (!done) {
 		frame = camera.read();
 		console.log('empty');
 	} else {
-		cv.imshow('frame', detect(frame.resize(240, 400)));
+		cv.imshow('frame', detect(frame.resize(340, 500)));
 		const key = cv.waitKey(delay);
 	}
 }
